663cf42c9ceeca50c75b5fcf970d4a1648f777a9
Albert Skalt <allber971@gmail.com>
2022-08-12 13:43:30 +0300

    why every time when i made .gitignore it doesnt work??

--- src/checker.cpp
+++ src/checker.cpp
@@ -9,88 +9,35 @@
 #include <cassert>
 #include <stack>
 #include <random>
-#include <deque>
+#include <bitset>
 #include <chrono>
 #include <map>
+#include <cstring>
 #define forn(i, n) for (int i = 0; i < n; ++i)
 #define all(a) begin(a), end(a)
 #define int long long
 #define sqr(x) ((x) * (x))
 
-#ifndef _debug
-    #define debug(...) 42
-    #define debr(...) 42
-#else
-    #define debug(x) cout << #x << " = " << x << endl
-    #define debr(...) { cout << #__VA_ARGS__ << " = ";  _debr(__VA_ARGS__); }
-#endif
-
 using namespace std;
-
-template<class Iter> 
-void _debr(Iter l, Iter r) {
-    cout << "[";
-    for (auto it = l; it != r; ++it) {
-        if (it != l) {
-            cout << ", ";   
-        }
-        cout << *it;
-    }
-    cout << "]" << endl;
-}
-
-template<class C, class F> 
-ostream& operator<<(ostream &out, pair<C, F> p) {
-    return out << "(" << p.first << ", " << p.second << ")";
-}
-
 using ll = long long;
 
-void hui(string s) {
-    cout << s << "\n";
-    exit(0);
-}
-
-map<pair<int, int>, int> color;
-int n, m;
-vector<string> a;
-
-void dfs(int i, int j, int predi = -1, int predj = -1) {
-    //cout << i << " " << j << "\n";
-    if (i < 0 || j < 0 || i >= n || j >= m) return;
-    if (a[i][j] != 'X') return;
-    if (color[{i, j}] == 2) return;
-    if (color[{i, j}] == 1) {
-        cout << 0 << "\n";
-        exit(0);
-    }
-    color[{i,j}] = 1;
-    if (!(i-1 == predi && j == predj)) dfs(i-1, j, i, j);
-    if (!(i+1 == predi && j == predj)) dfs(i+1, j, i, j);
-    if (!(i == predi && j - 1 == predj)) dfs(i, j-1, i, j);
-    if (!(i == predi && j + 1 == predj)) dfs(i, j+1, i, j);
-    color[{i,j}] = 2;
-}
+#ifdef _debug
+#include "lib/debug.h"
+#else
+#define debug(...) 42
+#endif
 
 signed main() {
     ios::sync_with_stdio(false);
-    cin.tie(0);
+    cin.tie(0);             
 
-    cin >> n >> n >> m;
-    a.resize(n);
-    forn(i, n) cin >> a[i];
+    int a, b;
+    cin >> a >> b;
+    int myres;
+    cin >> myres;
+    int normres;
+    cin >> normres;
+    cout << (a + b == myres) << "\n";
 
-    forn(i, n) {
-        cin >> a[i];
-    }
-
-    forn(i, n - 1) {
-        forn(j, m - 1) {
-            if (a[i][j] == 'X' && !color.count({i, j})) {
-                dfs(i, j);
-            }
-        }
-    }
-    cout << 1 << "\n";
     return 0;
 }
